ffc29b9bbb82d814b285d741ea656384f2a7de84
foolment <mr.kingzon@gmail.com>
2015-05-18 12:53:26 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Youdao.wox
-A wox plugin using youdao api to translate input text. Python implement version.
+A wox plugin using youdao api to translate input text. Python implement version from [Wox.Plugin.Youdao](https://github.com/qianlifeng/Wox.Plugin.Youdao), adding proxy support.
--- youdao.py
+++ youdao.py
@@ -7,19 +7,80 @@ import webbrowser
 import requests
 from wox import Wox
 
-NODE_ICO_PATH = r'img\node'
+import chardet
 
 QUERY_API = 'http://fanyi.youdao.com/openapi.do?keyfrom=WoxYoudaoPlugin&key=315576122&type=data&doctype=json&version=1.1&q='
+EMPTY_RESULT = {
+  'Title': 'Start to translate between Chinese and English',
+  'SubTitle': 'Powered by youdao api',
+  'IcoPath': 'Img\\youdao.ico'
+}
 
 class Main(Wox):
   
   def query(self, param):
-    response = requests.get(QUERY_API + param.strip()).json()
-
+    result = []
+    if param.strip() == '':
+      result.append(EMPTY_RESULT)
+    else:
+      response = requests.get(QUERY_API + param.strip(), proxies = self.__get_proxies()).json()
+      if response and response['errorCode'] == 0:
+        if 'translation' in response and response['translation'] != []:
+          result.append({
+            'Title': ','.join(response['translation']),
+            'SubTitle': '有道翻译',
+            'IcoPath': 'Img\\youdao.ico',
+            'JsonRPCAction': {
+              'method': 'open_url',
+              'parameters': [param.strip()]
+            }
+          })
+        if 'basic' in response and response['basic'] != {}:
+          for i in response['basic']['explains']:
+            result.append({
+              'Title': i,
+              'SubTitle': '{} - 基本词典'.format(response['query'].encode('utf-8')),
+              'IcoPath': 'Img\\youdao.ico',
+              'JsonRPCAction': {
+                'method': 'open_url',
+                'parameters': [param.strip()]
+              }
+            })
+        if 'web' in response and response['web'] != []:
+          for i in response['web']:
+            result.append({
+              'Title': ','.join(i['value']),
+              'SubTitle': '{} - 网络释义'.format(i['key'].encode('utf-8')),
+              'IcoPath': 'Img\\youdao.ico',
+              'JsonRPCAction': {
+                'method': 'open_url',
+                'parameters': [param.strip()]
+              }
+            })
+      elif response:
+        errCode = response['errorCode']
+        errMsg = '未知错误'
+        if errCode == 20:
+          errMsg = '要翻译的文本过长'
+        elif errCode == 30:
+          errMsg = '无法进行有效的翻译'
+        elif errCode == 40:
+          errMsg = '不支持的语言类型'
+        elif errCode == 50:
+          errMsg = '无效的key'
+        elif errCode == 60:
+          errMsg = '无词典结果'
+        result.append({
+          'Title': errMsg,
+          'SubTitle': 'errorCode=%d' % errCode,
+          'IcoPath': 'Img\\youdao.ico'
+        })
+      else:
+        result.append(EMPTY_RESULT)
     return result
 
-  def open_url(self, url):
-    webbrowser.open(url)
+  def open_url(self, query):
+    webbrowser.open('http://dict.youdao.com/search?q=' + query)
 
   def __get_proxies(self):
     proxies = {}
